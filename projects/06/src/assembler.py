7e52df693586196715eff3e5de66230d858ebc8b
Dileep Rajput <dileeprajput686@gmail.com>
2021-11-23 16:46:15 -0500

    Add support for symbols in assembler

--- projects/06/src/assembler.py
+++ projects/06/src/assembler.py
@@ -2,13 +2,34 @@ import sys
 from modules.Parser import Parser
 from modules.Code import Code
 from modules.CommandTypes import CommandTypes
+from modules.SymbolTable import SymbolTable
 
 def print_usage():
     print("python assembler.py <source>.asm <out>.hack")
 
-def assemble(infile, outfile):
+def first_pass(infile) -> SymbolTable:
+    table = SymbolTable()
+    instruction_addr = 0
     for line in infile.readlines():
+        #clean and skip lines
+        clean_line = Parser.clean(line)
+        if len(clean_line) == 0:
+            continue
 
+        command_type = Parser.commandType(clean_line)
+        if (command_type == CommandTypes.A_COMMAND or command_type == CommandTypes.C_COMMAND):
+            instruction_addr += 1
+        elif (command_type == CommandTypes.L_COMMAND):
+            symbol = Parser.symbol(clean_line)
+            table.addEntry(symbol, instruction_addr)
+        else:
+            raise ValueError("got a invalid command: " + clean_line)
+
+    return table
+
+def second_pass(infile, outfile, table):
+    ram_address = 16
+    for line in infile.readlines():
         #clean and skip lines
         clean_line = Parser.clean(line)
         if len(clean_line) == 0:
@@ -16,21 +37,35 @@ def assemble(infile, outfile):
         
         command_type = Parser.commandType(clean_line)
         bin = ""
+
         if (command_type == CommandTypes.A_COMMAND):
             symbol = Parser.symbol(clean_line)
-            bin = Code.a2bin(int(symbol))
+            if symbol.isnumeric():
+                symbol = int(symbol)
+            else:
+                if table.contains(symbol):
+                    symbol = table.getAddress(symbol)
+                else:
+                    table.addEntry(symbol, ram_address)
+                    symbol = ram_address
+                    ram_address += 1
+            bin = Code.a2bin(symbol)
         elif (command_type == CommandTypes.C_COMMAND):
             dest = Parser.dest(clean_line)
             comp = Parser.comp(clean_line)
             jump = Parser.jump(clean_line)
             bin = Code.c2bin(comp, dest, jump)
         elif (command_type == CommandTypes.L_COMMAND):
-            symbol = Parser.symbol(clean_line)
-            bin = Code.a2bin(int(symbol))
+            continue
         else:
             raise ValueError("got a invalid command: " + clean_line)
 
         outfile.write(bin + "\n")
+
+def assemble(infile, outfile):
+    table = first_pass(infile)
+    infile.seek(0)
+    second_pass(infile, outfile, table)
 
 def main():
     if len(sys.argv) != 3:
--- projects/06/src/modules/SymbolTable.py
+++ projects/06/src/modules/SymbolTable.py
@@ -1,13 +1,40 @@
 class SymbolTable:
 
     def __init__(self):
-        pass
+        self.table = {
+            "SP" : 0,
+            "LCL" : 1,
+            "ARG" : 2,
+            "THIS" : 3,
+            "THAT" : 4,
+            "R0" : 0,
+            "R1" : 1,
+            "R2" : 2,
+            "R3" : 3,
+            "R4" : 4,
+            "R5" : 5,
+            "R6" : 6,
+            "R7" : 7,
+            "R8" : 8,
+            "R9" : 9,
+            "R10" : 10,
+            "R11" : 11,
+            "R12" : 12,
+            "R13" : 13,
+            "R14" : 14,
+            "R15" : 15,
+            "SCREEN" : 16384,
+            "KBD": 24576
+        }
 
     def addEntry(self, symbol: str, address: int):
-        pass
+        self.table[symbol] = address
 
     def contains(self, symbol: str) -> bool:
-        pass
+        return symbol in self.table.keys()
 
     def getAddress(self, symbol: str) -> int:
-        pass
+        return self.table[symbol]
+
+    def __str__(self) -> str:
+        return self.table.__str__()
